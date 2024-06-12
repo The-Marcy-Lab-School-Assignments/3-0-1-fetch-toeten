a341e5dddcddcbd62a2646cdc5cdc3f1bb2cd39b
toeten <n.santiago0228@gmail.com>
2024-06-11 21:18:55 -0400

    answered qs 1 and 2

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,9 +1,25 @@
 const userUrl = 'https://jsonplaceholder.typicode.com/users'
 
 export const checkResponseStatus = () => {
+  return fetch(userUrl)
+  .then(response => {
+    return {
+      status: response.status,
+      ok: response.ok,
+      url: response.url
+    }
+  })
+  .catch(error => {
+    console.error('yo this aint right')
+  })
 };
 
 export const getUsers = () => {
+  return fetch(userUrl)
+  .then(response => {
+    return response.json()
+  })
+  .then(users => users)
 };
 
 export const getUserPosts = () => {
