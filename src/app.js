966033be9476faf5b45f28fde3588d57b0fbd471
toeten <n.santiago0228@gmail.com>
2024-06-12 13:35:40 -0400

    did app part

--- src/app.js
+++ src/app.js
@@ -13,4 +13,49 @@ import {
 } from './fetch-functions.js';
 
 export default function app(appDiv) {
+  //dont forget setUpPageBasics from render file........
+  const { statusDiv, usersUl, postsUl, newUserForm, newUserDiv } = setupPageBasics(appDiv);
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      const statusInfo = {
+        url: 'https://jsonplaceholder.typicode.com/users',
+        status: response.status,
+        ok: response.ok
+      }
+      renderStatus(statusDiv, statusInfo)
+    })
+
+  
+  getUsers()
+    .then(users => {
+      renderUsers(usersUl, users)
+    })
+
+  
+  usersUl.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+      const userId = event.target.getAttribute('data-user-id')
+      getUserPosts(userId)
+        .then(posts => {
+          renderPosts(postsUl, posts)
+        })
+    }
+  });
+
+ 
+  newUserForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const formData = new FormData(newUserForm)
+    const newUserData = {
+      username: formData.get('username'),
+      email: formData.get('email')
+    };
+
+    createNewUser(newUserData)
+      .then(newUser => {
+        renderNewUser(newUserDiv, newUser)
+        newUserForm.reset()
+      })
+  });
 }
